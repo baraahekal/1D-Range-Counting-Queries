404d178dd5cd64d3b4a1501361b816f9721976f6
baraahekal <ibaraahekal@gmail.com>
2023-12-28 18:31:50 +0200

    Fixing some index bugs

--- 1D_range_query_binarySearch.cpp
+++ 1D_range_query_binarySearch.cpp
@@ -6,34 +6,30 @@ int binary_search(vector<int> &arr, int target) {
     int low = 0;
     int high = (int)arr.size() - 1;
    
-    
-    int idx = 0;
+    int mid = 0;
     while (low <= high) {
-        int mid = (high + low) / 2;
-        idx = mid;
+        mid = (high + low) / 2;
         
-        if (target > arr[mid] && mid + 1 < arr.size() && arr[mid + 1] < target) {
+        if (target > arr[mid] && mid + 1 < arr.size() && arr[mid + 1] <= target) {
             low = mid + 1;
         }
-        else if (target < arr[mid] && mid - 1 >= 0 && arr[mid - 1] > target) {
+        else if (target < arr[mid] && mid - 1 >= 0 && arr[mid - 1] >= target) {
             high = mid - 1;
-            idx = mid;
         }
         else {
             break;
         }
     }
-    return idx;
+    return mid;
 }
 
 int32_t main() {
-    vector<int> x {4,8,11,13,17,21,27};
-    int low = 7;
-    int high = 21;
+    vector<int> x  {1, 3, 5, 7, 9, 11, 13, 15};
+    int low = 4;
+    int high = 10;
     
     int idx_low = binary_search(x, low);
     int idx_high = binary_search(x, high);
     
-    cout << idx_high - (x[idx_low] >= low ? idx_low : idx_low + 1) + 1 << el;
+    cout << (x[idx_high] <= high ? idx_high : idx_high - 1) - (x[idx_low] >= low ? idx_low : idx_low + 1) + 1 << el;
 }
-
